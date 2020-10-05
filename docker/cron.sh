3192d1e849e65f194c752ada06aaf8507a0b08d3
Maximilian Pfundstein <maximilian@pfundstein.org>
2020-10-05 16:50:48 +0200

    Bugfix in cron.sh

--- docker/cron.sh
+++ docker/cron.sh
@@ -9,16 +9,9 @@ if [ "$PAALIAQ_RUN_ONCE" = true ] ; then
     echo "$@"
     echo "Command line arguments should have been printed."
 
-    # TODO: Add call to run your application only ONCE during testing
-    # $@ holds all command line arguments added to docker run container...
-    # So there must be a way to pass a different connection string to your
-    # application for testing. An environment variable would also work
-    # the test is okay, if your application returns 0, therefor this parts
-    # runs your application ocnce and if there was no error, it returns 0.
+    python main.py $@
 
-    #./bors-data/main.py $@
-
-    if [[ $? != 0 ]]; then
+    if [ $? != 0 ]; then
     echo "Error occured while running application only once."
         exit 1
     fi
